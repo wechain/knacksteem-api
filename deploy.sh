eee8bfa01e6148db8baaf23f84d0d836bf01886c
wechain <anybucket@outlook.com>
2018-09-03 18:18:54 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t danielfsousa/express-rest-es2017-boilerplate .
-docker push danielfsousa/express-rest-es2017-boilerplate
+docker build -t casteem/express-rest-es2017-boilerplate .
+docker push casteem/express-rest-es2017-boilerplate
 
 ssh deploy@$DEPLOY_SERVER << EOF
-docker pull danielfsousa/express-rest-es2017-boilerplate
+docker pull casteem/express-rest-es2017-boilerplate
 docker stop api-boilerplate || true
 docker rm api-boilerplate || true
-docker rmi danielfsousa/express-rest-es2017-boilerplate:current || true
-docker tag danielfsousa/express-rest-es2017-boilerplate:latest danielfsousa/express-rest-es2017-boilerplate:current
-docker run -d --restart always --name api-boilerplate -p 3000:3000 danielfsousa/express-rest-es2017-boilerplate:current
+docker rmi casteem/express-rest-es2017-boilerplate:current || true
+docker tag casteem/express-rest-es2017-boilerplate:latest danielfsousa/express-rest-es2017-boilerplate:current
+docker run -d --restart always --name api-boilerplate -p 3000:3000 casteem/express-rest-es2017-boilerplate:current
 EOF
